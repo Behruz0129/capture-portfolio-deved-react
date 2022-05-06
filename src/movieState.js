78e8eb3c915998ef38a822959cfa34db080f4b16
Behruz Berdiyev <73114092+Behruz0129@users.noreply.github.com>
2022-05-06 14:58:54 +0500

    post3

--- src/movieState.js
+++ src/movieState.js
@@ -3,16 +3,18 @@ import kangaroo1 from './img/projects/kangaroo1.jpg';
 import kangaroo2 from './img/projects/kangaroo2.jpg';
 import iphoneseven1 from './img/projects/iphoneseven1.gif'
 import iphoneseven2 from './img/projects/iphoneseven.png'
+import spidermonkey1 from './img/projects/spider-monkey1.png'
+import spidermonkey2 from './img/projects/spider-monkey2.jpg'
 
 export const MovieState = () => {
   return [
     {
-      name: 'Kangaroo',
-      mainImg: kangaroo1,
-      secondaryImg: kangaroo2,
-      url: '/projects/kangaroo',
+      name: 'Spider Monkey',
+      mainImg: spidermonkey1,
+      secondaryImg: spidermonkey2,
+      url: '/projects/spidermonkey',
       type: 'photo',
-      id: 1,
+      id: 2,
       awards: [
         {
           title: 'About Project',
@@ -32,7 +34,7 @@ export const MovieState = () => {
       secondaryImg: iphoneseven2,
       url: '/projects/iphoneseven',
       type: 'video',
-      id: 0,
+      id: 1,
       awards: [
         {
           title: 'About Project',
@@ -43,6 +45,26 @@ export const MovieState = () => {
           title: 'What I did',
           description:
             'The prototype of the video can be used to make other commercials.',
+        },
+      ],
+    },
+    {
+      name: 'Kangaroo',
+      mainImg: kangaroo1,
+      secondaryImg: kangaroo2,
+      url: '/projects/kangaroo',
+      type: 'photo',
+      id: 0,
+      awards: [
+        {
+          title: 'About Project',
+          description:
+            'This model can be used in the field of cinematography, game applications. The model`s grid is low poly.',
+        },
+        {
+          title: 'What I did',
+          description:
+            'This model used a particle system and was created in 1 day. The part has been edited and colored in a realistic way.',
         },
       ],
     },
