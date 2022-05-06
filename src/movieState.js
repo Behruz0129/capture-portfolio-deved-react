ccf63aaf9817fd46284a0cdcae81c1b8f09b0918
Behruz Berdiyev <73114092+Behruz0129@users.noreply.github.com>
2022-05-06 14:31:00 +0500

    post

--- src/movieState.js
+++ src/movieState.js
@@ -1,6 +1,8 @@
 //import images
 import kangaroo1 from './img/projects/kangaroo.jpg';
 import kangaroo2 from './img/projects/kangaroo.jpg';
+import iphoneseven1 from './img/projects/iphoneseven1.gif'
+import iphoneseven2 from './img/projects/iphoneseven.png'
 
 export const MovieState = () => {
   return [
@@ -10,7 +12,7 @@ export const MovieState = () => {
       secondaryImg: kangaroo2,
       url: '/projects/kangaroo',
       type: 'photo',
-      id: 0,
+      id: 1,
       awards: [
         {
           title: 'About Project',
@@ -25,22 +27,22 @@ export const MovieState = () => {
       ],
     },
     {
-      name: 'Kangaroo',
-      mainImg: kangaroo1,
-      secondaryImg: kangaroo2,
-      url: '/projects/kangaroo',
-      type: 'photo',
+      name: 'iPhone Trailer',
+      mainImg: iphoneseven1,
+      secondaryImg: iphoneseven2,
+      url: '/projects/iphoneseven',
+      type: 'video',
       id: 0,
       awards: [
         {
           title: 'About Project',
           description:
-            'This model can be used in the field of cinematography, game applications. The model`s grid is low poly.',
+            'The video was made as an alternative to the iPhone trailer. Different color phone models were used in the animation.',
         },
         {
           title: 'What I did',
           description:
-            'This model used a particle system and was created in 1 day. The part has been edited and colored in a realistic way.',
+            'The prototype of the video can be used to make other commercials.',
         },
       ],
     },
